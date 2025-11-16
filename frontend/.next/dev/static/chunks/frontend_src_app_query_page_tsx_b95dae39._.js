2e8590a440446237d1be71a505c3da9c19304d01
Anubhav Agarwal <anubhavagarwal.agarwal8@gmail.com>
2025-11-16 22:10:40 +0530

    README update

--- frontend/.next/dev/static/chunks/frontend_src_app_query_page_tsx_b95dae39._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/frontend_5a0c0d9f._.js"
-],
-    source: "dynamic"
-});
